d378414ecb67972f49283494561eda3cd8e2aedf
Mazharrais <mazharmajo72@gmail.com>
2024-10-05 12:37:25 +0500

    add code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Todo from './Components/Todo/Todo';
 
 function App() {
   return (
     <div className="App">
-  <h1>WelCome tO React Todo App...!</h1>
+    <Todo />
     </div>
   );
 }
---
+++ src/Components/Todo/Todo.jsx
@@ -0,0 +1,18 @@
+
+
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <>
+      <section className='todo-container'>
+        <header>
+            <h1>TodoList</h1>
+        </header>
+
+      </section>
+    </>
+  )
+}
+
+export default Todo
